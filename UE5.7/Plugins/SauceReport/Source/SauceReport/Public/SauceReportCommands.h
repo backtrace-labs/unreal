75f747e4bdc96c6cc6c7ae49e09f39697d23dc70
Juan Belon <juaxix@gmail.com>
2025-12-21 18:43:50 +0100

    Adding UE5.7 project. - Tested in Mac.

---
+++ UE5.7/Plugins/SauceReport/Source/SauceReport/Public/SauceReportCommands.h
@@ -0,0 +1,22 @@
+// Copyright Sauce Labs, Inc. All Rights Reserved.
+
+#pragma once
+
+#include <Framework/Commands/Commands.h>
+
+#include "SauceReportStyle.h"
+
+class FSauceReportCommands : public TCommands<FSauceReportCommands>
+{
+public:
+
+	FSauceReportCommands()
+		: TCommands<FSauceReportCommands>(TEXT("SauceReport"), NSLOCTEXT("Contexts", "SauceReport", "SauceReport Plugin"), NAME_None, FSauceReportStyle::GetStyleSetName())
+	{
+	}
+
+	// TCommands<> interface
+	virtual void RegisterCommands() override;
+
+	TSharedPtr<FUICommandInfo> PluginAction{nullptr};
+};
---
+++ UE5.7/Source/SauceSample.Target.cs
@@ -0,0 +1,15 @@
+// Sauce Labs Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SauceSampleTarget : TargetRules
+{
+	public SauceSampleTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+
+		ExtraModuleNames.AddRange( new string[] { "SauceSample" } );
+	}
+}
